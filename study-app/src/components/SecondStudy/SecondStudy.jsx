f3090ffbdbf968b1b6b1c5675b15c3bb216e0993
Dubabbi <thdms3160@naver.com>
2024-10-24 22:02:40 +0900

    Study: useEffect

--- study-app/src/components/SecondStudy/SecondStudy.jsx
+++ study-app/src/components/SecondStudy/SecondStudy.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function SecondStudy () {
+const [count, setCount] = useState(1);
+const handleCountUpdate = () => {
+  setCount(count + 1);
+};
+useEffect(() =>{
+  console.log('렌더링!');
+})
 
   return (
-    <></>
+    <>
+      <button onClick={handleCountUpdate}>Update</button>
+      <span>count: {count}</span>
+
+    </>
   );
 }
 
